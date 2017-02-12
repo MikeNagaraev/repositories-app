5e41b4ec7c2f9ce99e38bfd7cc46651f05ab529b
MikhailLU <clowfinger13@gmail.com>
2017-02-12 18:28:56 +0300

    MEAN working

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -3,26 +3,47 @@ var app = angular.module('flapperNews', ['ui.router']);
 app.controller('MainCtrl', ['$scope', 'postsFactory',
     function($scope, postsFactory) {
         $scope.posts = postsFactory.posts;
+
         $scope.addPost = function() {
-            postsFactory.addPost($scope.title, $scope.link)
+            if (!$scope.title || $scope.title === '') {
+                return;
+            }
+            postsFactory.create({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0,
+                comments: []
+            })
             $scope.title = ''
             $scope.link = ''
         }
+
         $scope.incrementUpvotes = function(post) {
-            postsFactory.incrementUpvotes(post);
+            postsFactory.upvote(post);
         }
     }
 ]);
 
-app.controller('PostsCtrl', ['$scope', '$stateParams', 'postsFactory',
-    function($scope, $stateParams, postsFactory) {
-        $scope.post = postsFactory.posts[$stateParams.id];
+app.controller('PostsCtrl', ['$scope', 'post', 'postsFactory', 'commentsService',
+    function($scope, post, postsFactory, commentsService) {
+        $scope.post = post;
         $scope.addComment = function() {
-            postsFactory.addComment($scope.post, $scope.comment)
+            if ($scope.comment === '') {
+                return;
+            }
+            postsFactory.addComment(post._id, {
+                    body: $scope.comment,
+                    author: 'anonimous',
+                    upvotes: 0
+                })
+                .success(function(comment) {
+                    $scope.post.comments.push(comment);
+                })
             $scope.comment = ''
         }
-        $scope.incrementUpvotes = function(comment) {
-            postsFactory.incrementUpvotes(comment);
+
+        $scope.incrementUpvotes = function(post, comment) {
+            commentsService.upvote(post, comment);
         }
     }
 ]);
@@ -37,133 +58,67 @@ app.config([
                 url: '/home',
                 templateUrl: 'partials/home.html',
                 controller: 'MainCtrl',
-                // controllerAs: 'X'
+                resolve: {
+                    postPromise: ['postsFactory', function(posts) {
+                        return posts.getAll();
+                    }]
+                }
             })
             .state('posts', {
                 url: '/posts/{id}',
                 templateUrl: 'partials/posts.html',
-                controller: 'PostsCtrl'
+                controller: 'PostsCtrl',
+                resolve: {
+                    post: ['$stateParams', 'postsFactory', function($stateParams, posts) {
+                        return posts.get($stateParams.id);
+                    }]
+                }
             });
 
         $urlRouterProvider.otherwise('home');
     }
 ]);
 
-app.factory('postsFactory', function() {
-    var store = {}
-    store.posts = [{
-            title: 'post1',
-            upvotes: 5,
-            comments: [{
-                    author: 'Joe',
-                    body: 'Cool post!',
-                    upvotes: 0
-                },
-                {
-                    author: 'Bob',
-                    body: 'Great idea but everything is wrong!',
-                    upvotes: 0
-                }
-            ]
-        },
-        {
-            title: 'post2',
-            upvotes: 5,
-            comments: [{
-                    author: 'Joe',
-                    body: 'Cool post!',
-                    upvotes: 0
-                },
-                {
-                    author: 'Bob',
-                    body: 'Great idea but everything is wrong!',
-                    upvotes: 0
-                }
-            ]
-        },
-        {
-            title: 'post3',
-            upvotes: 2,
-            comments: [{
-                    author: 'Joe',
-                    body: 'Cool post!',
-                    upvotes: 0
-                },
-                {
-                    author: 'Bob',
-                    body: 'Great idea but everything is wrong!',
-                    upvotes: 0
-                }
-            ]
-        },
-        {
-            title: 'post4',
-            upvotes: 3,
-            comments: [{
-                    author: 'Joe',
-                    body: 'Cool post!',
-                    upvotes: 0
-                },
-                {
-                    author: 'Bob',
-                    body: 'Great idea but everything is wrong!',
-                    upvotes: 0
-                }
-            ]
-        },
-        {
-            title: 'post5',
-            upvotes: 5,
-            comments: [{
-                    author: 'Joe',
-                    body: 'Cool post!',
-                    upvotes: 0
-                },
-                {
-                    author: 'Bob',
-                    body: 'Great idea but everything is wrong!',
-                    upvotes: 0
-                }
-            ]
-        },
-    ]
+app.service('commentsService', ['$http', function($http) {
+    this.upvote = function(post, comment) {
+        return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+            .success(function(data) {
+                comment.upvotes += 1;
+            })
+    }
+}])
 
-    store.addPost = function(title, link) {
-        if (!title || title === '') {
-            return;
-        }
-        store.posts.push({
-            title: title,
-            link: link,
-            upvotes: 0,
-            comments: [{
-                    author: 'Joe',
-                    body: 'Cool post!',
-                    upvotes: 0
-                },
-                {
-                    author: 'Bob',
-                    body: 'Great idea but everything is wrong!',
-                    upvotes: 0
-                }
-            ]
+app.factory('postsFactory', ['$http', function($http) {
+    var store = {
+        posts: []
+    }
+    store.getAll = function() {
+        return $http.get('/posts').success(function(data) {
+            angular.copy(data, store.posts);
         })
     }
 
-    store.addComment = function(post, comment) {
-        if (comment === '') {
-            return;
-        }
-        post.comments.push({
-            author: 'Mikhail',
-            body: comment,
-            upvotes: 0
+    store.get = function(id) {
+        return $http.get('/posts/' + id).then(function(res) {
+            return res.data;
         })
     }
 
-    store.incrementUpvotes = function(item) {
-        item.upvotes += 1;
+    store.create = function(post) {
+        return $http.post('/posts', post).success(function(data) {
+            store.posts.push(data)
+        })
     }
 
+    store.upvote = function(post) {
+        return $http.put('/posts/' + post._id + '/upvote')
+            .success(function(data) {
+                post.upvotes += 1;
+            });
+    }
+
+    store.addComment = function(id, comment) {
+        return $http.post('/posts/' + id + '/comments', comment);
+    }
     return store;
-})
+}])
